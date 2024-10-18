c8c97daf7ad539255a47f7f99ca108b2b6bab43c
Mingjian Wen <wenxx151@gmail.com>
2024-10-18 00:07:31 -0500

    Add tests for double_factorial

--- tests/test_tensor_product.py
+++ tests/test_tensor_product.py
@@ -1,4 +1,34 @@
-from carten.tensor_product import get_permutations, tp_rule_even
+from math import factorial as factorial_math
+
+from carten.tensor_product import (
+    double_factorial,
+    factorial,
+    get_permutations,
+    tp_rule_even,
+    tp_rule_odd,
+)
+
+
+def test_factorial():
+    for i in range(10):
+        assert factorial(i) == factorial_math(i)
+
+
+def test_double_factorial():
+    assert double_factorial(0) == 1
+    assert double_factorial(1) == 1
+    assert double_factorial(2) == 2
+    assert double_factorial(3) == 3
+    assert double_factorial(4) == 8
+    assert double_factorial(5) == 15
+    assert double_factorial(6) == 48
+    assert double_factorial(7) == 105
+    assert double_factorial(8) == 384
+    assert double_factorial(9) == 945
+    assert double_factorial(10) == 3840
+
+    assert double_factorial(8, lower_bound=4) == 192
+    assert double_factorial(9, lower_bound=5) == 315
 
 
 def test_get_permutations():
@@ -27,20 +57,34 @@ def test_tp_rule_even():
 
     rule, symmetry = tp_rule_even(4, 4, 3, 0)
     assert rule == "abcd,abce->de"
-    assert symmetry == "ab"
+    assert symmetry == "xy"
 
     rule, symmetry = tp_rule_even(4, 4, 3, 1)
     assert rule == "abcd,abcd,ef->ef"
-    assert symmetry == "cc"
+    assert symmetry == "aa"
 
     rule, symmetry = tp_rule_even(4, 4, 2, 0)
     assert rule == "abcd,abef->cdef"
-    assert symmetry == "aabb"
+    assert symmetry == "xxyy"
 
     rule, symmetry = tp_rule_even(4, 4, 2, 1)
     assert rule == "abcd,abce,fg->defg"
-    assert symmetry == "abcc"
+    assert symmetry == "xyaa"
 
     rule, symmetry = tp_rule_even(4, 4, 2, 2)
     assert rule == "abcd,abcd,ef,gh->efgh"
-    assert symmetry == "ccdd"
+    assert symmetry == "aabb"
+
+
+def test_tp_rule_odd():
+    rule, symmetry = tp_rule_odd(4, 4, 3, 0)
+    assert rule == "abc,bdef,cdef->a"
+    assert symmetry == "x"
+
+    rule, symmetry = tp_rule_odd(4, 4, 2, 0)
+    assert rule == "abc,bdef,cdeg->afg"
+    assert symmetry == "xyz"
+
+    rule, symmetry = tp_rule_odd(4, 4, 2, 1)
+    assert rule == "abc,bdef,cdef,gh->agh"
+    assert symmetry == "xaa"
