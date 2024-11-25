f892111000aa9f450c1d50b43c8013e34cebe33f
Mingjian Wen <wenxx151@gmail.com>
2024-11-25 12:16:49 -0600

    Remove old unique code

--- tests/test_unique.py
+++
@@ -1,9 +0,0 @@
-from carten.unique import epsilon_T
-
-
-def test_epsilon_T():
-    out = epsilon_T(1, "ijkl")
-    assert out == ["23kl", "-32kl"]
-
-    out = epsilon_T(2, "kimjn")
-    assert out == ["k3m1n", "-k1m3n"]
