8bafbe3de08c312f58f8ae0aa499c5561f66bfe3
Mingjian Wen <wenxx151@gmail.com>
2024-12-03 11:20:19 -0600

    Add test for get_nt_from_vector

---
+++ tests/core/test_unit_vector.py
@@ -0,0 +1,47 @@
+import torch
+
+from carten.core.unit_vector import get_nt_from_vector, get_nt_from_vector_rule
+from carten.core.utils import is_symmetric_traceless
+
+
+def test_get_nt_from_vector_rule():
+    rule, symmetry, delta_indices = get_nt_from_vector_rule(3, 0)
+    assert rule == "a,b,c->abc"
+    assert symmetry == "xxx"
+    assert delta_indices == ""
+
+    rule, symmetry, delta_indices = get_nt_from_vector_rule(3, 1)
+    assert rule == "a,bc->abc"
+    assert symmetry == "xaa"
+    assert delta_indices == "a"
+
+    rule, symmetry, delta_indices = get_nt_from_vector_rule(4, 0)
+    assert rule == "a,b,c,d->abcd"
+    assert symmetry == "xxxx"
+    assert delta_indices == ""
+
+    rule, symmetry, delta_indices = get_nt_from_vector_rule(4, 1)
+    assert rule == "a,b,cd->abcd"
+    assert symmetry == "xxaa"
+    assert delta_indices == "a"
+
+    rule, symmetry, delta_indices = get_nt_from_vector_rule(4, 2)
+    assert rule == "ab,cd->abcd"
+    assert symmetry == "aabb"
+    assert delta_indices == "ab"
+
+
+def test_get_nt_from_vector():
+    a = torch.tensor([1.0, 2.0, 3.0])
+    a = a / a.norm()
+
+    nt = get_nt_from_vector(a, 0)
+    assert nt == torch.tensor(1.0)
+
+    nt = get_nt_from_vector(a, 1)
+    assert torch.allclose(nt, a)
+
+    for n in range(2, 5):
+        nt = get_nt_from_vector(a, n)
+        assert nt.shape == tuple([3] * n)
+        assert is_symmetric_traceless(nt), f"Failing for n = {n}"
