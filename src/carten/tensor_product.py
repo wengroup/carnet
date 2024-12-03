8d55e5b7e2df76a5b245a6772e3728dd22356f2d
Mingjian Wen <wenxx151@gmail.com>
2024-12-02 22:02:30 -0600

    Fix tp_odd

--- src/carten/tensor_product.py
+++ src/carten/tensor_product.py
@@ -4,11 +4,10 @@ Ref:
 [LP89] "Angular reduction in multiparticle matrix elements" by D. R. Lehman and W. C. Parke.
 http://dx.doi.org/10.1063/1.528515
 """
-import itertools
-
 import torch
 from torch import Tensor
 
+from carten.permute import get_permutations_delta
 from carten.utils import (
     dij,
     double_factorial,
@@ -55,10 +54,14 @@ def tp_even(X: Tensor, Y: Tensor, out_rank: int, normalize: str = None) -> Tenso
             2 * l3 - 1, 2 * l3 - 2 * t - 1 + 2, device=device
         )
 
-        rule, symmetry = tp_rule_even(l1, l2, k, t)
+        rule, symmetry, delta_indices = tp_rule_even(l1, l2, k, t)
+
+        # get one tensor product
         prod = torch.einsum(rule, X, Y, *([d] * t))
 
-        prod = symmetrize(prod, l1, l2, t, k)
+        # get all tensor products by symmetrizing the one tensor product
+        perms = get_permutations_delta(symmetry, delta_indices)
+        prod = torch.sum(torch.stack([prod.permute(p) for p in perms]), dim=0)
 
         out = out + coeff * prod
 
@@ -103,15 +106,18 @@ def tp_odd(X: Tensor, Y: Tensor, out_rank: int, normalize: str = None) -> Tensor
     epsilon = eijk(device)
 
     for t in range(min(l1, l2) - k):
-        # TODO, since in symmetrize() we already considers average over all possible
-        #  permutations, it seems the **m is not needed here.
         coeff = (-2) ** t / double_factorial(
             2 * l3 - 1, 2 * l3 - 2 * t - 1 + 2, device=device
         )
 
-        rule, symmetry = tp_rule_odd(l1, l2, k, t)
+        rule, symmetry, delta_indices = tp_rule_odd(l1, l2, k, t)
+
+        # get one tensor product
         prod = torch.einsum(rule, epsilon, X, Y, *([d] * t))
-        prod = symmetrize(prod, symmetry=symmetry, mode="sum")
+
+        # get all tensor products by symmetrizing the one tensor product
+        perms = get_permutations_delta(symmetry, delta_indices)
+        prod = torch.sum(torch.stack([prod.permute(p) for p in perms]), dim=0)
 
         out = out + coeff * prod
 
@@ -191,34 +197,33 @@ def coeff_D(l1: int, l2: int, l3: int, device: torch.device = None):
     )
 
 
-# TODO, the symmetry can be simplified to retain fewer terms, if we consider the
-#  major symmetry of different deltas
-def tp_rule_even(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
+def tp_rule_even(l1: int, l2: int, k: int, t: int) -> tuple[str, str, str]:
     """
     Get the einsum rule when l1 + l2 - l3 is even.
 
-    x_l1 \otimes^{k+m} x_l2 \otimes I ^{\otimes^m}
+    x_l1 \odot^{k+t} x_l2 \otimes I ^{\otimes^m}
 
-    After contraction, the resultant tensor will have l1-k-m indices from x, and these
-    indices are still symmetric. Similarly, the resultant tensor will have l2-k-m
-    symmetric indices from y. It will have 2*m indices from I. Each two indices from I
+    After contraction, the resultant tensor will have l1-k-t indices from x, and these
+    indices are still symmetric. Similarly, the resultant tensor will have l2-k-t
+    symmetric indices from y. It will have 2*t indices from I. Each two indices from I
     are symmetric.
 
-    In total, the resultant tensor will have l3 = l1 + l2 - 2k - 2m indices.
+    In total, the resultant tensor will have l3 = l1 + l2 - 2(k + t) indices.
 
     Returns:
         rule: The einsum rule for the tensor product
         symmetry: The symmetry information of the resultant tensor after the tensor
             product. e.g. `xxxyyyaa` means the first three indices are symmetric, the
             next three indices are symmetric, and the last two indices are symmetric.
+        delta_indices: The indices for the delta tensors.
     """
 
     xy_contracted = letter_index(k + t)
     x_remain = letter_index(l1 - k - t, k + t)
     y_remain = letter_index(l2 - k - t, l1)
 
-    # indices for contracting m I
-    # x and y uses l1 + l2 - k - m indices
+    # indices for contracting t of I
+    # x and y uses l1 + l2 - k - t indices
     delta = double_index(t, l1 + l2 - k - t)
     delta_left = "," + ",".join(delta) if delta else ""
     delta_right = "".join(delta)
@@ -235,25 +240,25 @@ def tp_rule_even(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
         "x" * len(x_remain) + "y" * len(y_remain) + "".join(repeat_double_index(t))
     )
 
-    return rule, symmetry
+    delta_indices = letter_index(t)
+
+    return rule, symmetry, delta_indices
 
 
-# TODO, the symmetry can be simplified to retain fewer terms, if we consider the
-#  major symmetry of different deltas
-def tp_rule_odd(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
+def tp_rule_odd(l1: int, l2: int, k: int, t: int) -> tuple[str, str, str]:
     """
     Get the einsum rule when l1 + l2 - l3 is odd.
 
-    epsilon : x_l1 \otimes^{k+m} x_l2 \otimes I ^{\otimes^m}
+    epsilon : x_l1 \odot^{k+t} x_l2 \otimes I ^{\otimes^t}
 
     epsilon is the Levi-Civita symbol. It contracts away one index from x and one index
     from y. So, after contraction, the resultant tensor will have 1 index from epsilon.
-    After contraction, the resultant tensor will have l1-1-k-m indices from x, and these
-    indices are still symmetric. Similarly, the resultant tensor will have l2-1-k-m
+    After contraction, the resultant tensor will have l1-1-k-t indices from x, and these
+    indices are still symmetric. Similarly, the resultant tensor will have l2-1-k-t
     symmetric indices from y. It will have 2*m indices from I. Each two indices from I
     are symmetric.
 
-    In total, the resultant tensor will have l3 = l1 + l2 - 1 - 2k - 2m indices.
+    In total, the resultant tensor will have l3 = l1 + l2 - 1 - 2(k + t) indices.
 
 
     Returns:
@@ -261,6 +266,7 @@ def tp_rule_odd(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
         symmetry: The symmetry information of the resultant tensor after the tensor
             product. e.g. `aabb` means the first two indices are symmetric, and the last
             two indices are symmetric.
+        delta_indices: The indices for the delta tensors.
     """
     # example: epsilon_abc x_bpqr  y_cpqs I_tu -> arstu
 
@@ -270,8 +276,8 @@ def tp_rule_odd(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
     x_remain = letter_index(l1 - 1 - k - t, k + t + 3)
     y_remain = letter_index(l2 - 1 - k - t, l1 + 2)
 
-    # indices for contracting m I
-    # x and y uses l1 + l2 - k - m indices
+    # indices for contracting t of I
+    # x and y uses l1 + l2 - k - t + 1 indices
     delta = double_index(t, l1 + l2 - k - t + 1)
     delta_left = "," + ",".join(delta) if delta else ""
     delta_right = "".join(delta)
@@ -282,9 +288,9 @@ def tp_rule_odd(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
     )
 
     # 1 index from epsilon
-    # l1-1-k-m remaining symmetric indices from x
-    # l2-1-k-m remaining symmetric indices from y
-    # 2m indices from all deltas. Each delta has 2 symmetric indices.
+    # l1-1-k-t remaining symmetric indices from x
+    # l2-1-k-t remaining symmetric indices from y
+    # 2t indices from all deltas. Each delta has 2 symmetric indices.
     symmetry = (
         "x"
         + "y" * len(x_remain)
@@ -292,149 +298,9 @@ def tp_rule_odd(l1: int, l2: int, k: int, t: int) -> tuple[str, str]:
         + "".join(repeat_double_index(t))
     )
 
-    return rule, symmetry
+    delta_indices = letter_index(t)
 
-
-def symmetrize(
-    Z: Tensor, l1: int, l2: int, t: int, k: int, start_dim: int = 0
-) -> Tensor:
-    """
-    Symmetrize a tensor that is obtained as: Z =  X \odot^(k+t) Y \otimes \delta^t
-    where X and Y are symmetric tensors, and \delta is the delta tensor.
-
-    Args:
-        Z: the Z tensor
-        l1: the rank of the first symmetric tensor X
-        l2: the rank of the second symmetric tensor Y
-        t: the number of delta tensors
-        k: k+t the number of indices contracted between X and Y
-        start_dim: the starting dimension to perform the operation. Dimensions before
-            `start_dim` will not be used in the operation.
-
-
-    Returns:
-        A symmetrized tensor.
-    """
-
-    # Get unique permutations
-    permutations = get_permutations(l1, l2, t, k, start_dim)
-
-    # Sum over the permutations
-    u = torch.sum(torch.stack([Z.permute(p) for p in permutations]), dim=0)
-
-    return u
-
-
-# TODO, this can be combined with get_permutations_2 in reduce.py
-# We provide symmetry as input.
-
-
-def get_permutations(
-    l1: int, l2: int, t: int, k: int, start_dim: int = 0
-) -> list[list[int]]:
-    """
-    Get unique permutations of the tensor product between two symmetric tensors X and Y
-    and delta tensors.
-
-    Z =  X \odot^(k+t) Y \otimes \delta^t
-
-    For example, given X_abce and Y_fghi, when k=0 and t=2, then output tensor Z is a
-    rank 8 tensor (l1+l2 - 2(k+t) + 2t).
-
-    We have: Z_cdefghij = X_abcd Y_abef delta_gh delta_ij as one of the terms.
-    Will will have C(8, 4) * C(4, 2) * 3 = 70*6*3 =1260 unique permutations.
-
-    First, we consider indices cdef:
-        - cd is symmetric
-        - ef is symmetric
-    So we have 6 permutations for cdef (4 chose 2):
-        cdef, cedf, cefd, ecdf, ecfd, efcd
-    Second, we consider indices ghij:
-        - gh is symmetric
-        - ij is symmetric
-        - gh and ij are symmetric because both gh and ij are associated with delta
-    So, we need to consider both minor and major symmetries for the delta tensors, and
-    we have 3 permutations for ghij:
-        ghij, gihj, gijh
-
-    All together, we have 1260 unique permutations: choosing 4 indices from 8 to arrange
-    the indices of cdef (and the other 4 indices are for ghij); then 6 permutations for
-    cdef, and 3 permutations for ghij.
-
-    So, we consider three types of symmetries:
-        1. minor symmetry in delta tensors
-        2. major symmetry in delta tensors
-        3. symmetries in the remaining indices of X and Y
-
-    Args:
-        l1: the rank of the first symmetric tensor X
-        l2: the rank of the second symmetric tensor Y
-        t: the number of delta tensors
-        k: k+t the number of indices contracted between X and Y
-        start_dim: the starting dimension to perform the operation. Dimensions before
-            `start_dim` will not be used in the operation.
-
-    Returns:
-        Each inner list contains the permutation indices for symmetrization.
-    """
-
-    def canonize(ps: str) -> str:
-        """
-        Convert a permutation string to its canonical form, such that equivalent
-        permutation strings have the same representation.
-
-        This considers:
-          - both minor symmetry and major symmetry in delta tensors  (indices a, b, ...)
-          - symmetries in the remaining indices of X and Y (indices x, y)
-
-        This is based on first occurrence positions of each letter in the string.
-        For example, `baba` and `fefe` are equivalent permutation strings, and
-        both will be converted to `0101`.
-
-        Args:
-            ps: The permutation string to convert
-
-        Returns:
-            The canonical form of the permutation string
-        """
-
-        # Do not need to convert x and y to numerical values, because, for example,
-        # xxyy and yyxx are different.
-        exclude = {"x", "y"}
-        return "".join(c if c in exclude else str(ps.index(c)) for c in ps)
-
-    num_x_remain = l1 - k - t
-    num_y_remain = l2 - k - t
-    assert (
-        num_x_remain >= 0 and num_y_remain >= 0
-    ), "Both l1 and l2 must be greater than or equal to k + t"
-
-    # Construct the symmetry pattern
-    # e.g., xxyyaabb, meaning we have 2 symmetric indices for X, 2 symmetric indices
-    # for Y, and 2 pairs of symmetric indices from two deltas.
-
-    x_remain = "x" * (l1 - k - t)
-    y_remain = "y" * (l2 - k - t)
-    delta = "".join(repeat_double_index(t))
-
-    symmetry = f"{x_remain}{y_remain}{delta}"
-
-    all_perms = itertools.permutations(range(start_dim, start_dim + len(symmetry)))
-
-    prefix = list(range(start_dim))
-    unique_perms = []
-    unique_canonical_forms = set()
-
-    # Filter permutations based on contraction pattern
-    for perm in all_perms:
-        perm_string = "".join(symmetry[i - start_dim] for i in perm)
-
-        canonical_form = canonize(perm_string)
-        if canonical_form not in unique_canonical_forms:
-            unique_perms.append(prefix + list(perm))
-            unique_canonical_forms.add(canonical_form)
-
-    return unique_perms
+    return rule, symmetry, delta_indices
 
 
 if __name__ == "__main__":
--- tests/test_tensor_product.py
+++ tests/test_tensor_product.py
@@ -3,41 +3,41 @@ from carten.utils import is_symmetric, is_symmetric_traceless, is_traceless
 
 
 def test_tp_rule_even():
-    rule, symmetry = tp_rule_even(4, 4, 4, 0)
+    rule, symmetry, _ = tp_rule_even(4, 4, 4, 0)
     assert rule == "abcd,abcd->"
     assert symmetry == ""
 
-    rule, symmetry = tp_rule_even(4, 4, 3, 0)
+    rule, symmetry, _ = tp_rule_even(4, 4, 3, 0)
     assert rule == "abcd,abce->de"
     assert symmetry == "xy"
 
-    rule, symmetry = tp_rule_even(4, 4, 3, 1)
+    rule, symmetry, _ = tp_rule_even(4, 4, 3, 1)
     assert rule == "abcd,abcd,ef->ef"
     assert symmetry == "aa"
 
-    rule, symmetry = tp_rule_even(4, 4, 2, 0)
+    rule, symmetry, _ = tp_rule_even(4, 4, 2, 0)
     assert rule == "abcd,abef->cdef"
     assert symmetry == "xxyy"
 
-    rule, symmetry = tp_rule_even(4, 4, 2, 1)
+    rule, symmetry, _ = tp_rule_even(4, 4, 2, 1)
     assert rule == "abcd,abce,fg->defg"
     assert symmetry == "xyaa"
 
-    rule, symmetry = tp_rule_even(4, 4, 2, 2)
+    rule, symmetry, _ = tp_rule_even(4, 4, 2, 2)
     assert rule == "abcd,abcd,ef,gh->efgh"
     assert symmetry == "aabb"
 
 
 def test_tp_rule_odd():
-    rule, symmetry = tp_rule_odd(4, 4, 3, 0)
+    rule, symmetry, _ = tp_rule_odd(4, 4, 3, 0)
     assert rule == "abc,bdef,cdef->a"
     assert symmetry == "x"
 
-    rule, symmetry = tp_rule_odd(4, 4, 2, 0)
+    rule, symmetry, _ = tp_rule_odd(4, 4, 2, 0)
     assert rule == "abc,bdef,cdeg->afg"
     assert symmetry == "xyz"
 
-    rule, symmetry = tp_rule_odd(4, 4, 2, 1)
+    rule, symmetry, _ = tp_rule_odd(4, 4, 2, 1)
     assert rule == "abc,bdef,cdef,gh->agh"
     assert symmetry == "xaa"
 
@@ -66,4 +66,9 @@ def test_tp_odd(NT3, NT4):
     for i in [2, 4, 6]:
         out = tp_odd(NT3, NT4, out_rank=i)
         assert out.ndim == i
-        assert is_symmetric_traceless(out, atol=1e-5), f"Failed for {i}"
+        assert is_symmetric_traceless(out, atol=1e-4), f"Failed for {i}"
+
+    for i in [1, 3, 5, 7]:
+        out = tp_odd(NT4, NT4, out_rank=i)
+        assert out.ndim == i
+        assert is_symmetric_traceless(out, atol=1e-4), f"Failed for {i}"
