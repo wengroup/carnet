1c079b0a02c7eee8b093b147ca9dcbc727fbda49
Mingjian Wen <wenxx151@gmail.com>
2025-04-08 12:24:22 +0800

    Add test for tabulate.py

---
+++ tests/symbolic/test_tabulate.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pytest
+
+from carten.symbolic.tabulate import get_G_H_S
+
+
+# TODO, n=1 does not work
+@pytest.mark.parametrize("n", [2, 3, 4])
+def test_get_G_H_S(n):
+    """Test the get_G_H_S function.
+
+    For a given tensor T, obtain the natural tensors X (using H), and then obtain the
+    embedding T' in the original tensor space (using G). We check that we can recover T.
+
+    Args:
+        n: rank of the tensor T
+    """
+    np.random.seed(35)
+
+    T = np.random.randn(*[3] * n)
+
+    output = get_G_H_S(n)
+
+    all_T_prime = []
+    for j, out_j in output.items():
+
+        for p, (H, G, S) in enumerate(zip(out_j["H"], out_j["G"], out_j["S"])):
+            # X = H T
+            X = np.einsum(H["rule"], H["numerical"], T)
+
+            # T' = G X
+            T_p_1 = np.einsum(G["rule"], G["numerical"], X)
+
+            # T' = S T
+            T_p_2 = np.einsum(S["rule"], S["numerical"], T)
+
+            # T_p_1 and T_p_2 should be equal
+            assert np.allclose(T_p_1, T_p_2), (
+                f"T_p_1 and T_p_2 are not equal for j=" f"{j}, p={p}"
+            )
+
+            all_T_prime.append(T_p_1)
+
+    sum_T_prime = np.sum(all_T_prime, axis=0)
+
+    assert np.allclose(sum_T_prime, T)
