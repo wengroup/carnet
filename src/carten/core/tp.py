ed776dadfc69fc1d7b1424f3e1fa89d0ae04de3a
Mingjian Wen <wenxx151@gmail.com>
2025-05-21 10:58:31 +0800

    Move tp rule and coeff to natt

--- src/carten/core/tp.py
+++ src/carten/core/tp.py
@@ -7,15 +7,12 @@ Ref:
 http://dx.doi.org/10.1063/1.528515
 """
 
-from typing import Optional
-
 import torch
 from line_profiler import profile
+from natt.H_tp import coeff_C, coeff_D, get_tp_even_rule, get_tp_odd_rule
 from natt.symmetrize import get_permutations_delta, symmetrize_via_permutation
-from natt.utils import dij, double_index, eijk, letter_index, repeat_double_index
+from natt.utils import dij, double_factorial, eijk
 from torch import Tensor
-
-from carten.core.utils import double_factorial, factorial
 
 # CACHE to speed up calculation, they will be filled when the functions are called
 TP_EVEN_RULE_CACHE = {}
@@ -168,6 +165,8 @@ def tp_odd(
 
         global TP_ODD_RULE_CACHE
         if (l1, l2, k, t) not in TP_ODD_RULE_CACHE:
+            # TODO, can move the calling of get_permutations_delta into
+            #  get_tp_odd_rule and such
             rule, symmetry, delta_indices = get_tp_odd_rule(l1, l2, k, t)
             TP_ODD_RULE_CACHE[(l1, l2, k, t)] = (rule, symmetry, delta_indices)
         else:
@@ -209,168 +208,14 @@ def tp_odd(
     return Z
 
 
-def coeff_C(l1: int, l2: int, l3: int, device: Optional[torch.device] = None):
-    """Coefficient C for even L.
+if __name__ == "__main__":
+    from natt.symmetrize import get_random_natural_tensor
 
-    The coefficient is obtained such at l3 fold contraction of the output tensor with
-    A unit vector yields 1.
+    l1 = 4
+    l2 = 4
+    l3 = 2
 
-    Ref: Eq. 54 of [LP89]
-    """
-    L = l1 + l2 + l3
-    L1 = L - 2 * l1 - 1
-    L2 = L - 2 * l2 - 1
-    L3 = L - 2 * l3 - 1
+    X = get_random_natural_tensor(l1, seed=1).view(-1)
+    Y = get_random_natural_tensor(l2, seed=2).view(-1)
 
-    return (
-        factorial(l1, device)
-        * factorial(l2, device)
-        * double_factorial(2 * l3 - 1, device=device)
-        * factorial((L1 + 1) // 2, device=device)
-        * factorial((L2 + 1) // 2, device=device)
-        / factorial(l3, device=device)
-        / double_factorial(L1, device=device)
-        / double_factorial(L2, device=device)
-        / double_factorial(L3, device=device)
-        / factorial(L // 2, device=device)
-    )
-
-
-def coeff_D(l1: int, l2: int, l3: int, device: Optional[torch.device] = None):
-    """Coefficient D for odd L.
-
-    The coefficient is obtained such at l3 fold contraction of the output tensor with
-    A unit vector yields 1.
-
-    Ref: Eq. 55 of [LP89]
-    """
-    L = l1 + l2 + l3
-    L1 = L - 2 * l1 - 1
-    L2 = L - 2 * l2 - 1
-    L3 = L - 2 * l3 - 1
-
-    return (
-        2
-        * factorial(l1, device)
-        * factorial(l2, device)
-        * double_factorial(2 * l3 - 1, device=device)
-        * factorial(L1 // 2, device=device)
-        * factorial(L2 // 2, device=device)
-        / factorial(l3 - 1, device=device)
-        / double_factorial(L1 + 1, device=device)
-        / double_factorial(L2 + 1, device=device)
-        / double_factorial(L3 + 1, device=device)
-        / factorial((L + 1) // 2, device=device)
-    )
-
-
-def get_tp_even_rule(l1: int, l2: int, k: int, t: int) -> tuple[str, str, str]:
-    """
-    Get the einsum rule when l1 + l2 - l3 is even.
-
-    x_l1 \odot^{k+t} x_l2 \otimes I ^{\otimes^m}
-
-    After contraction, the resultant tensor will have l1-k-t indices from x, and these
-    indices are still symmetric. Similarly, the resultant tensor will have l2-k-t
-    symmetric indices from y. It will have 2*t indices from I. Each two indices from I
-    are symmetric.
-
-    In total, the resultant tensor will have l3 = l1 + l2 - 2(k + t) tensor indices.
-
-    Returns:
-        rule: The einsum rule for the tensor product
-        symmetry: The symmetry information of the resultant tensor after the tensor.
-            product. e.g. `xxxyyyaa` means the first three indices are symmetric, the
-            next three indices are symmetric, and the last two indices are symmetric.
-        delta_indices: The indices for the delta tensors.
-    """
-
-    # indices that are contracted
-    xy_contracted = letter_index(k + t)
-
-    # indices that are not contracted
-    x_remain = letter_index(l1 - k - t, k + t)
-    y_remain = letter_index(l2 - k - t, l1)
-
-    # indices for contracting t of I
-    delta = double_index(t, upper_case=True)
-    delta_left = "," + ",".join(delta) if delta else ""
-    delta_right = "".join(delta)
-
-    rule = (
-        f"...{xy_contracted}{x_remain},"
-        f"...{xy_contracted}{y_remain}"
-        f"{delta_left}"
-        f"->...{x_remain}{y_remain}{delta_right}"
-    )
-
-    # l1-k-t remaining symmetric indices from x
-    # l2-k-t remaining symmetric indices from y
-    # 2t indices from all deltas. Each delta has 2 symmetric indices.
-    # TorchScript does not allow string multiplication, so we need to use `join`
-    symmetry = (
-        "".join(["a"] * len(x_remain))
-        + "".join(["b"] * len(y_remain))
-        + "".join(repeat_double_index(t, upper_case=True))
-    )
-    delta_indices = letter_index(t, upper_case=True)
-
-    return rule, symmetry, delta_indices
-
-
-def get_tp_odd_rule(l1: int, l2: int, k: int, t: int) -> tuple[str, str, str]:
-    """
-    Get the einsum rule when l1 + l2 - l3 is odd.
-
-    epsilon : x_l1 \odot^{k+t} x_l2 \otimes I ^{\otimes^t}
-
-    epsilon is the Levi-Civita symbol. It contracts away one index from x and one index
-    from y. So, after contraction, the resultant tensor will have 1 index from epsilon.
-    After contraction, the resultant tensor will have l1-1-k-t indices from x, and these
-    indices are still symmetric. Similarly, the resultant tensor will have l2-1-k-t
-    symmetric indices from y. It will have 2*m indices from I. Each two indices from I
-    are symmetric.
-
-    In total, the resultant tensor will have l3 = l1 + l2 - 1 - 2(k + t) indices.
-
-
-    Returns:
-        rule: The einsum rule for the tensor product
-        symmetry: The symmetry information of the resultant tensor after the tensor
-            product. e.g. `aabb` means the first two indices are symmetric, and the last
-            two indices are symmetric.
-        delta_indices: The indices for the delta tensors.
-    """
-    # example: epsilon_uvw x_vabc  y_wabd I_AB -> ucdAB
-
-    xy_contracted = letter_index(k + t)
-    x_remain = letter_index(l1 - 1 - k - t, k + t)
-    y_remain = letter_index(l2 - 1 - k - t, l1 - 1)
-
-    # indices for contracting t of I
-    delta = double_index(t, upper_case=True)
-    delta_left = "," + ",".join(delta) if delta else ""
-    delta_right = "".join(delta)
-
-    # The ... are for the batch dimensions
-    rule = (
-        f"uvw,"  # indices for epsilon
-        f"...v{xy_contracted}{x_remain},"
-        f"...w{xy_contracted}{y_remain}"
-        f"{delta_left}"
-        f"->...u{x_remain}{y_remain}{delta_right}"
-    )
-
-    # 1 index from epsilon
-    # l1-1-k-t remaining symmetric indices from x
-    # l2-1-k-t remaining symmetric indices from y
-    # 2t indices from all deltas. Each delta has 2 symmetric indices.
-    symmetry = (
-        "a"
-        + "".join(["b"] * len(x_remain))
-        + "".join(["c"] * len(y_remain))
-        + "".join(repeat_double_index(t, upper_case=True))
-    )
-    delta_indices = letter_index(t, upper_case=True)
-
-    return rule, symmetry, delta_indices
+    tp_even(X, Y, l1, l2, l3)
