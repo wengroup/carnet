1289b55dae7310932c98450b10d51183dd8a1f9c
Mingjian Wen <wenxx151@gmail.com>
2025-05-22 09:44:05 +0800

    Add json dump and load with gz compression

--- src/carten/utils.py
+++ src/carten/utils.py
@@ -1,3 +1,7 @@
+import gzip
+import json
+from pathlib import Path
+
 import torch
 from torch import Tensor, nn
 
@@ -61,3 +65,40 @@ class BufferDict(nn.Module):
 
     def items(self):
         return self.named_buffers()
+
+
+def json_dump(obj: dict, filename: Path, compress: bool = True) -> None:
+    """Dump a dictionary to a json file.
+
+    Args:
+        obj: The dictionary to dump.
+        filename: The path to the json file.
+        compress: How to compress the file. If None, no compression is used. Options
+            are: `gz` and `xz`.
+
+
+    """
+    if compress:
+        filename = filename.with_suffix(filename.suffix + ".gz")
+        with gzip.open(filename, "wt") as f:
+            json.dump(obj, f)
+    else:
+        with open(filename, "w") as f:
+            json.dump(obj, f)
+
+
+def json_load(filename: Path) -> dict:
+    """Load a json file into a dictionary.
+
+    Args:
+        filename: The path to the json file.
+
+    Returns:
+        The loaded dictionary.
+    """
+    if filename.suffix == ".gz":
+        with gzip.open(filename, "rt") as f:
+            return json.load(f)
+    else:
+        with open(filename, "r") as f:
+            return json.load(f)
