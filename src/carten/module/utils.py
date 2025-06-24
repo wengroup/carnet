39afb466cdc6f9d417c25f08c4e94c302393799b
Mingjian Wen <wenxx151@gmail.com>
2025-06-24 22:03:42 +0800

    Add `camp`, `lite` path mode

--- src/carten/module/utils.py
+++ src/carten/module/utils.py
@@ -36,21 +36,16 @@ def get_paths(
         L1: maximum rank of the natural tensor in the first input feature tensor.
         L2: maximum rank of the natural tensor in the second input feature tensor.
         L3: ranks of the output feature tensor.
-        mode: how to compute the paths. Supported modes are `full`, `mid`, `lite`.
+        mode: how to compute the paths. Supported modes are `full`, `lite`, and `camp`.
             In `full` mode, all paths satisfying `abs(l1 - l2) <= l3 <= l1 + l2` are
             generated.
-            In `mid` mode, the upper bound is constrained to `max(l1, l2)`, meaning
-            paths satisfying `abs(l1 - l2) <= l3 <= max(l1, l2)` are generated. By
-            doing this, information are allowed to flow from high-rank tensors to
-            low-rank tensors, but not the other way around.
-            # TODO, the description of lite is incorrect.
-            In `lite` mode, the upper bound is set to `abs(l1-l2)`, the same as lower.
-            The is going to be exactly the same rule as in `CAMP`. The idea is
-            that `tensor contraction` (instead of tensor product), is used.
-            For example, given l1=2, l2=2: `full` mode will give l3 in [0, 1, 2, 3, 4];
-            `mid` mode will give l3 in [0, 1, 2], not exceeding the ranks of the input
-           tensors; `lite` mode will only allow l3 in [0, 2], corresponding to
-           contraction once or twice of the indices of input tensors.
+            The `camp` is the same rule as used in the CAMP model.
+            In `lite` mode, the tensors X of rank l1 and Y of rank l2 are to be
+            contracted by rank k. Same as the `camp` mode, it is required that
+            l3 = l1 + l2 - 2 * k, but unlike `camp`, where it is required that
+            l1<l2, here, we do not require that. Also, `camp` requires l1 to be fully
+            contracted, while `lite` does not.
+            In terms of the number of allowed paths, full>lite>camp.
 
     Returns:
         Dictionary of paths from L1 and L2 to L3: {l3: [(l1, l2, l3)]}, where each
@@ -60,26 +55,23 @@ def get_paths(
 
     for l1 in range(L1 + 1):
         for l2 in range(L2 + 1):
-            if mode in ["full", "mid"]:
-                if mode == "full":
-                    upper = l1 + l2 + 1
-                else:
-                    upper = max(l1, l2) + 1
-
-                for l in range(abs(l1 - l2), upper):
+            if mode == "full":
+                for l in range(abs(l1 - l2), l1 + l2 + 1):
                     if l in L3:
                         paths[l].append((l1, l2, l))
-            # TODO, modify the docs above, this is not the same as in CAMP, where the
-            #  lower-rank tensors are fully contracted away, meaning l1=2 and l2=4
-            #  will only lead to l3=2, (contracting both indices of l1); and it is
-            #  impossible to have l3=4 (contracting only one index of l1).
             elif mode == "lite":
-                for i in range(1, (l1 + l2) // 2 + 1):
+                for i in range((l1 + l2) // 2 + 1):
                     if i > l1 or i > l2:
                         continue
                     l = l1 + l2 - 2 * i
                     if l in L3:
                         paths[l].append((l1, l2, l))
+            elif mode == "camp":
+                if l1 <= l2:
+                    l = l1 + l2 - 2 * l1
+                    if l in L3:
+                        paths[l].append((l1, l2, l))
+
             else:
                 raise ValueError(f"Invalid mode: {mode}.")
 
@@ -87,9 +79,13 @@ def get_paths(
 
 
 if __name__ == "__main__":
-    paths1 = get_paths(4, 4, [0, 1, 2, 3, 4], mode="full")
-    paths2 = get_paths(4, 4, [0, 1, 2, 3, 4], mode="mid")
-    paths3 = get_paths(4, 4, [0, 1, 2, 3, 4], mode="lite")
-    pprint(paths1)
-    pprint(paths2)
-    pprint(paths3)
+
+    L1 = 3
+    L2 = 3
+    L3 = list(range(3 + 1))
+    for mode in ["full", "lite", "camp"]:
+        paths = get_paths(L1, L2, L3, mode)
+        print("\n" + "#" * 40)
+        print(f"mode: {mode}")
+        print({k: len(v) for k, v in paths.items()})
+        pprint(paths)
