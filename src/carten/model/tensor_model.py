09479f155409c414a7618364abac0854610287d5
Mingjian Wen <wenxx151@gmail.com>
2025-06-25 09:05:51 +0800

    Add tp_path_mode to StructureTensor

--- src/carten/model/tensor_model.py
+++ src/carten/model/tensor_model.py
@@ -172,6 +172,7 @@ class StructureTensorModel(nn.Module):
         max_chebyshev_degree: int = 8,
         radial_mlp_hidden_layers: list[int] | int = 2,
         atomic_moment_mode: str = "vanilla",
+        tp_path_mode: str = "full",
         #
         layer_norm: bool = True,
         activation: str = "silu",
@@ -209,6 +210,7 @@ class StructureTensorModel(nn.Module):
             max_chebyshev_degree=max_chebyshev_degree,
             radial_mlp_hidden_layers=radial_mlp_hidden_layers,
             atomic_moment_mode=atomic_moment_mode,
+            tp_path_mode=tp_path_mode,
             layer_norm=layer_norm,
             activation=activation,
             last_layer_activation=last_layer_activation,
